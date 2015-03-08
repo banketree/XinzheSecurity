59ffb211a1f50c0f05a3abd9aa7808f65a67f186
linxinzhe <linxinzhechn@163.com>
2015-03-08 14:26:08 +0800

    Finish all function of anti-theft

---
+++ app/src/main/java/com/linxinzhe/android/xinzhesecurity/receiver/MyAdminReceiver.java
@@ -0,0 +1,15 @@
+package com.linxinzhe.android.xinzhesecurity.receiver;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyAdminReceiver extends DeviceAdminReceiver {
+    public MyAdminReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
--- app/src/main/java/com/linxinzhe/android/xinzhesecurity/receiver/SMSReceiver.java
+++ app/src/main/java/com/linxinzhe/android/xinzhesecurity/receiver/SMSReceiver.java
@@ -1,6 +1,8 @@
 package com.linxinzhe.android.xinzhesecurity.receiver;
 
+import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -18,6 +20,7 @@ public class SMSReceiver extends BroadcastReceiver {
     private static final String TAG = "SMSReceiver";
     private SharedPreferences sp;
 
+    private DevicePolicyManager dpm;
 
     public SMSReceiver() {
     }
@@ -34,39 +37,46 @@ public class SMSReceiver extends BroadcastReceiver {
             //判断是否是安全号码
             String safenumber = sp.getString("safenumber", "");
             if (sender.contains(safenumber)) {
-                if (body.contains("#*location*#")){
+                if (body.contains("#*location*#")) {
                     Log.i(TAG, "得到手机GPS");
                     Intent gpsIntent = new Intent(context, GPSService.class);
                     context.startService(gpsIntent);
                     SharedPreferences sp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
                     String lastLocationGPS = sp.getString("lastLocationGPS", "");
                     String lastLocationNET = sp.getString("lastLocationNET", "");
-                    if (TextUtils.isEmpty(lastLocationGPS)&&TextUtils.isEmpty(lastLocationNET)) {
+                    if (TextUtils.isEmpty(lastLocationGPS) && TextUtils.isEmpty(lastLocationNET)) {
                         //得不到位置
                         SmsManager.getDefault().sendTextMessage(sender, null, "获得位置中", null, null);
                     } else {
-                        SmsManager.getDefault().sendTextMessage(sender, null, lastLocationGPS+lastLocationNET, null, null);
+                        SmsManager.getDefault().sendTextMessage(sender, null, lastLocationGPS + lastLocationNET, null, null);
                     }
                     abortBroadcast();
-                }else if (body.contains("#*alarm*#")){
+                } else if (body.contains("#*alarm*#")) {
                     Log.i(TAG, "得到报警信号");
                     MediaPlayer player = MediaPlayer.create(context, R.raw.ylzs);
                     player.setLooping(false);
                     player.setVolume(1.0f, 1.0f);
                     player.start();
                     abortBroadcast();
-                }else if (body.contains("#*wipedata*#")){
+                } else if (body.contains("#*wipedata*#")) {
+
+                    dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+                    dpm.wipeData(DevicePolicyManager.WIPE_EXTERNAL_STORAGE);
+                    dpm.wipeData(0);
+                    abortBroadcast();
                     Log.i(TAG, "得到消除信号");
-
-                    abortBroadcast();
-                }else if (body.contains("#*lockscreen*#")){
+                } else if (body.contains("#*lockscreen*#")) {
                     Log.i(TAG, "得到锁屏信号");
+                    dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+                    ComponentName who = new ComponentName(context, MyAdminReceiver.class);
+                    if (dpm.isAdminActive(who)) {
+                        dpm.lockNow();
 
-                    abortBroadcast();
+                        dpm.resetPassword("123", 0);
+                        abortBroadcast();
+                    }
                 }
             }
         }
     }
-
-
 }
